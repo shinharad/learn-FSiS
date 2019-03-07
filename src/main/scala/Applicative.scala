3e8d68a090554f7565f25de3464597cf1f6dbae7
shinharad <shinharad.github@gmail.com>
2019-03-07 17:28:42 +0900

    part2

--- src/main/scala/Applicative.scala
+++ src/main/scala/Applicative.scala
@@ -1,8 +1,9 @@
 import simulacrum._
 
 import scala.language.higherKinds
+import scala.language.implicitConversions
 
-@typeclass trait Applicative[F[_]] extends Functor[F] {
+@typeclass trait Applicative[F[_]] extends Functor[F] { self =>
 
   def pure[A](a: A): F[A]
 
@@ -20,14 +21,27 @@ import scala.language.higherKinds
   def map3[A, B, C, Z](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => Z): F[Z] =
     apply(fa)(map2(fb, fc)((b, c) => a => f(a, b, c)))
 
-  def map4[A, B, C, D, Z](fa: F[A], fb: F[B], fc: F[C], fd: F[C])(f: (A, B, C, D) => Z): F[Z] =
-    apply(fa)(map3(fb, fc, fd)((b, c, d) => a => f(a, b, c, d)))
+  def map4[A, B, C, D, Z](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => Z): F[Z] =
+    map2(tuple2(fa, fb), tuple2(fc, fd)) { case ((a, b), (c, d)) => f(a, b, c, d) }
+//    apply(fa)(map3(fb, fc, fd)((b, c, d) => a => f(a, b, c, d)))
 
   def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
     map2(fa, fb)((a, b) => (a, b))
 
   def tuple3[A, B, C](fa: F[A], fb: F[B], fc: F[C]): F[(A, B, C)] =
     map3(fa, fb, fc)((a, b, c) => (a, b, c))
+
+  def flip[A, B](ff: F[A => B]): F[A] => F[B] =
+    fa => apply(fa)(ff)
+
+  def compose[G[_]](implicit G: Applicative[G]): Applicative[Lambda[X => F[G[X]]]] =
+    new Applicative[Lambda[X => F[G[X]]]] {
+      def pure[A](a: A): F[G[A]] = self.pure(G.pure(a))
+      def apply[A, B](fga: F[G[A]])(ff: F[G[A => B]]): F[G[B]] = {
+        val x: F[G[A] => G[B]] = self.map(ff)(gab => G.flip(gab))
+        self.apply(fga)(x)
+      }
+    }
 
 }
 
--- src/main/scala/part2_applicative.md
+++ src/main/scala/part2_applicative.md
@@ -48,3 +48,18 @@ res0: Option[(Int, Int)] = Some((1,2))
 scala> Applicative[List].tuple2(List(1, 2, 3), List(4, 5, 6))
 res1: List[(Int, Int)] = List((1,4), (1,5), (1,6), (2,4), (2,5), (2,6), (3,4), (3,5), (3,6))
 ```
+
+```scala
+scala> Applicative[Option].map4(Option(1), Option(2), Option(3), Option(4))(_ + _ + _ + _)
+res0: Option[Int] = Some(10)
+```
+
+```scala
+scala> for { x <- Option(1); y <- Option(2); z <- Option(3) } yield x + y + z
+res1: Option[Int] = Some(6)
+
+scala> Option(1).flatMap { x => Option(2).flatMap { y => Option(3).map { z => x + y + z } } }
+res3: Option[Int] = Some(6)
+```
+
+
